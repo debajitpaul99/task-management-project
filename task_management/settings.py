32a2a0577c02cb0e9e0631ddf50a317708c88e6e
Debajit Paul <debajit2003@gmail.com>
2026-03-10 21:56:38 +0600

    send email updated

--- task_management/settings.py
+++ task_management/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['task-management-project-jumu.onrender.com']
+ALLOWED_HOSTS = ['task-management-project-jumu.onrender.com', '127.0.0.1']
 
 
 # Application definition
@@ -166,7 +166,7 @@ EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='') 
 
-FRONTEND_URL = 'http://127.0.0.1:8000/'
+FRONTEND_URL = 'https://task-management-project-jumu.onrender.com/'
 
 LOGIN_URL = "/users/sign_in/"
 LOGIN_REDIRECT_URL = "home"
